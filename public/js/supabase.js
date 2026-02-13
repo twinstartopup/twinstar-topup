cf2b0a5e67f58f6961ea714fae85ed5514a97b5b
twinstartopup <twinstartopup@gmail.com>
2026-02-13 11:51:05 +0700

    Update supabase.js

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+window.supabase = createClient(
+  "https://ruygwnrzqbszlvntzcoa.supabase.co",
+  "sb_publishable_ZtbLiCQwl3ZjsPDAcv2QbA_9hwUf5mq"
+);
