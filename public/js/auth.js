1f7f9182a239c2d5bf0f1b406f7253f56b43c086
twinstartopup <twinstartopup@gmail.com>
2026-02-13 11:52:48 +0700

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,1 +1,26 @@
+import "./supabase.js";
 
+window.register = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  alert(error ? error.message : "Register berhasil!");
+};
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (!error) location.href = "/dashboard.html";
+  else alert(error.message);
+};
